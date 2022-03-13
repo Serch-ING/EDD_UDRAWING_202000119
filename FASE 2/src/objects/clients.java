8b629de5700ccf6b0e599afbca0e77c5bf87cfa8
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-03-13 17:09:55 -0600

    agregando arbol AVL

---
+++ FASE 2/src/objects/clients.java
@@ -0,0 +1,13 @@
+package objects;
+
+public class clients {
+	String Name,Password;
+	int DPI;
+	
+	public clients(String Name, String Password, int DPI) {
+		this.Name = Name;
+		this.Password = Password;
+		this.DPI = DPI;
+	}
+	
+}
