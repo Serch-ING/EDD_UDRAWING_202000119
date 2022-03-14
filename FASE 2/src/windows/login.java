4c930f720c8e54f4ff1bc5456657d411d69d7c5b
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-03-13 19:13:50 -0600

    validacion usuario admin
    Credenciales quemadas

--- FASE 2/src/windows/login.java
+++ FASE 2/src/windows/login.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.Font;
@@ -13,8 +14,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class login extends JFrame {
-	String passwordAdmin= "EDD2022", userAdmin ="admin";
-	
+	String passwordAdmin = "EDD2022", userAdmin = "admin";
+
 	private JPanel contentPane;
 	private JTextField UserText;
 	private JPasswordField passwordField;
@@ -46,38 +47,50 @@ public class login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Usuario:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel.setBounds(104, 95, 60, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		UserText = new JTextField();
 		UserText.setBounds(174, 94, 152, 20);
 		contentPane.add(UserText);
 		UserText.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(174, 125, 152, 20);
 		contentPane.add(passwordField);
-		
+
 		lblContrasea = new JLabel("Contrase\u00F1a:");
 		lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblContrasea.setBounds(82, 126, 82, 14);
 		contentPane.add(lblContrasea);
-		
+
 		JButton Button_join = new JButton("Ingresar");
 
 		Button_join.setBounds(237, 176, 89, 23);
 		contentPane.add(Button_join);
-		
-		
-		//Botenes
+
+		// Botenes
 		Button_join.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				try {
+					String password = String.valueOf(passwordField.getPassword());
+					String User = UserText.getText();
+
+					if (User.equals(userAdmin) && password.equals(passwordAdmin)) {
+						JOptionPane.showMessageDialog(null, "Ingreso");
+					} else {
+						JOptionPane.showMessageDialog(null, "Credenciales erroneas");
+					}
+
+				} catch (Exception e2) {
+					JOptionPane.showMessageDialog(null, "Ocurrio un error");
+				}
+
 			}
 		});
-		
+
 	}
 }
