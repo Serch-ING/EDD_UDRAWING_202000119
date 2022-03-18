4d378de4b927a340bd716ac061dcd2d327519ff7
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-03-17 19:54:28 -0600

    insert JFRAME for module clients

--- FASE 2/src/objects/Clients.java
+++ FASE 2/src/objects/Clients.java
@@ -1,13 +1,17 @@
 package objects;
 
+import storage.Arbol_AVL;
+
 public class Clients {
 	public String Name,Password;
 	public String DPI;
+	public Arbol_AVL AVL  = new Arbol_AVL();
 	
 	public Clients(String Name, String Password, String DPI) {
 		this.Name = Name;
 		this.Password = Password;
 		this.DPI = DPI;
+		
 	}
 	
 }
