89258d6c3e6f3e8c542481007b076d14c1f6b116
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-03-22 16:49:28 -0600

    creando interfaz basica carga masiva clientes

--- FASE 2/src/windows/Client_Module.java
+++ FASE 2/src/windows/Client_Module.java
@@ -6,6 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JTabbedPane;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 
 public class Client_Module extends JFrame {
 
@@ -31,12 +46,94 @@ public class Client_Module extends JFrame {
 	 * Create the frame.
 	 */
 	public Client_Module() {
+		JFileChooser fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("*.JSON", "JSON");
+		fc.setFileFilter(filter);
+		fc.setCurrentDirectory(new File("./Test"));
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1108, 609);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 41, 1072, 518);
+		contentPane.add(tabbedPane);
+
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Cargas masivas", null, panel, null);
+		panel.setLayout(null);
+
+		JButton Button_Load = new JButton("Cargar archivo");
+		
+		Button_Load.setBounds(10, 69, 122, 23);
+		panel.add(Button_Load);
+
+		JLabel lblNewLabel = new JLabel("Ruta:");
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(10, 117, 46, 14);
+		panel.add(lblNewLabel);
+
+		JLabel label_ruta = new JLabel("Null");
+		label_ruta.setForeground(Color.BLACK);
+		label_ruta.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		label_ruta.setBounds(48, 117, 569, 14);
+		panel.add(label_ruta);
+
+		JButton Button_loadData = new JButton("Cargar datos");
+
+		Button_loadData.setBounds(10, 160, 122, 23);
+		panel.add(Button_loadData);
+
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] { "Capas", "Imagenes", "Albums" }));
+		comboBox.setBounds(10, 24, 122, 22);
+		panel.add(comboBox);
+
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		panel_1.setLayout(null);
+
+		JButton btnNewButton = new JButton("Cerrar sesion");
+		btnNewButton.setBounds(964, 11, 118, 23);
+		contentPane.add(btnNewButton);
+
+		// buttons
+		
+		Button_Load.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (fc.showOpenDialog(Button_Load) == JFileChooser.APPROVE_OPTION) {
+					try {
+						label_ruta.setText(fc.getSelectedFile().toString());
+
+					} catch (Exception e2) {
+
+					}
+				} else {
+					label_ruta.setText("Null");
+				}
+			}
+		});
+		
+		Button_loadData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (label_ruta.getText() != "Null") {
+					if (comboBox.getSelectedItem() == "Capas") {
+						JOptionPane.showMessageDialog(null, "capas");
+					} else if (comboBox.getSelectedItem() == "Imagenes") {
+						JOptionPane.showMessageDialog(null, "Images");
+					} else if (comboBox.getSelectedItem() == "Albums") {
+						JOptionPane.showMessageDialog(null, "Albun");
+					}
+				}else {
+					JOptionPane.showMessageDialog(null, "Debe cargar un archivo");
+				}
+
+			}
+		});
+	}
 }
