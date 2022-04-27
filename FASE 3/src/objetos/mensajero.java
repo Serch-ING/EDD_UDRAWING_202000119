e004d39747414c5be787148c49f78d8d6148f1ff
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2022-04-26 20:07:08 -0600

    objeto mensajero

---
+++ FASE 3/src/objetos/mensajero.java
@@ -0,0 +1,18 @@
+package objetos;
+
+public class mensajero {
+	public long DPI; 
+	public String nombre,apellido,tipo_liciencia,genero,direccion;
+	public int telefono;
+	
+	public mensajero(long DPI,String nombre,String apellido,String tipo_liciencia,String genero,String direccion,int telefono) {
+		this.DPI= DPI;
+		this.nombre= nombre;
+		this.apellido= apellido;
+		this.tipo_liciencia= tipo_liciencia;
+		this.genero= genero;
+		this.direccion= direccion;
+		this.telefono= telefono;
+	
+	}
+}
